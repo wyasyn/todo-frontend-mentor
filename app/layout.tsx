f8adffe7cb46fbcb7bd9ae1661dc54299690b966
Yasin Walum <63132133+wyasyn@users.noreply.github.com>
2024-05-11 11:43:23 +0300

    Update layout.tsx
    added link to my wesite

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Josefin_Sans as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
+import Link from 'next/link'
 import Banner from "@/components/banner";
 import { ModeToggle } from "@/components/mode-toggle";
 import AddTodo from "@/components/addTodo";
@@ -59,7 +60,7 @@ export default function RootLayout({
                             </div>
 
                             <p className=" my-[7rem] text-center text-muted ">
-                                Drag and drop to reorder list
+                                Developed by <Link href="https://ywalum.com/">Yasin Walum</Link>
                             </p>
                         </section>
                     </main>
